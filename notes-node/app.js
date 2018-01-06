7d3985b7bb5b9be6c430ac2dbccad9fc5db6c4c6
Ashiqur Rahman <ashiqur15-5470@diu.edu.bd>
2018-01-07 04:44:25 +0600

    Lodash example

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,8 +1,23 @@
-console.log("Starting app.");
+console.log("Starting app.js");
 
 const fs = require("fs");
 const os = require("os");
+const _ = require("lodash");
 
-var user = os.userInfo();
+const notes = require("./notes");
 
-fs.appendFile("greetings.txt", "Hello " + user.username);
+console.log(_.isString(true));
+console.log(_.isString("Ashik"));
+
+var filteredArray = _.uniq(["Ashik", 1, "Ashik", 1, 2, 3, 4]);
+console.log(filteredArray);
+
+var res = notes.addNote();
+console.log(res);
+
+var result = notes.add(9, -2);
+console.log(result);
+
+//var user = os.userInfo();
+
+//fs.appendFile("greetings.txt", `Hello ${user.username}! You are ${notes.age}.`);
