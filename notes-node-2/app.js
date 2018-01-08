f68b8fbaf42c479950ebbe4fb654939aaf8b9a38
Ashiqur Rahman <ashiqur15-5470@diu.edu.bd>
2018-01-09 01:38:21 +0600

    Notes app completed

---
+++ notes-node-2/app.js
@@ -0,0 +1,66 @@
+//const fs = require("fs");
+//const _ = require("lodash");
+const notes = require("./notes");
+const yargs = require("yargs");
+
+const titleOptions = {
+  describe: "Title of note",
+  demand: true,
+  alias: "t"
+};
+
+const bodyOptions = {
+  describe: "Body of the note",
+  demand: true,
+  alias: "b"
+};
+
+const argv = yargs
+  .command("add", "Add a new note", {
+    title: titleOptions,
+    body: bodyOptions
+  })
+  .command("list", "List all notes")
+  .command("read", "Read a note", {
+    title: titleOptions
+  })
+  .command("remove", "remove a note", {
+    title: titleOptions
+  })
+  .help().argv;
+var command = argv._[0];
+
+// console.log("command: ", command);
+// // console.log("Process",process.argv);
+// console.log("Yargs ", argv);
+
+if (command === "add") {
+  var note = notes.addNote(argv.title, argv.body);
+  if (note) {
+    console.log("note created");
+    notes.logNote(note);
+  } else {
+    console.log("note title taken");
+  }
+} else if (command === "list") {
+  var allNotes = notes.getAll();
+  console.log(`Printing ${allNotes} note(s).`);
+  allNotes.forEach(note => {
+    notes.logNote(note);
+  });
+} else if (command === "read") {
+  var note = notes.getNote(argv.title);
+
+  if (note) {
+    console.log("Note found");
+    notes.logNote(note);
+  } else {
+    console.log("Note not found");
+  }
+} else if (command === "remove") {
+  var noteRemoved = notes.removeNote(argv.title);
+  var message = noteRemoved ? "Note was Removed" : "Note not found";
+  console.log(message);
+} else {
+  console.log("Command not recognized");
+}
---
+++ notes-node-2/playGround/json.js
@@ -0,0 +1,32 @@
+
+
+// var obj = {
+//     name: "Ashik"
+// };
+
+// var StringObj = JSON.stringify(obj);
+// console.log(typeof StringObj);
+// console.log(StringObj);
+
+// var personString = '{"name":"Ashik","age":25}';
+// var person = JSON.parse(personString);
+// console.log(typeof person);
+// console.log(person);
+
+const fs = require("fs");
+
+var originalNote = {
+    title: 'Some title',
+    body: 'Some body'
+};
+
+var originalNoteString = JSON.stringify(originalNote);
+fs.writeFileSync('notes.json',originalNoteString);
+
+var noteString = fs.readFileSync('notes.json');
+
+var note = JSON.parse(noteString);
+
+console.log(typeof note);
+console.log(note.title);
+
